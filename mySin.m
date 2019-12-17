626175335a254ce9b0b5f6176e398fc988d0d169
xdzurkova <xdzurkova@stuba.sk>
2019-12-17 02:37:31 +0100

    [minor]:nove

---
+++ mySin.m
@@ -0,0 +1,17 @@
+%% mySin
+%% Function domain
+x = -1:0.1:6.2;
+%% Function values
+y =sin(x)
+%% Figure initialization
+figure
+%% Plotting figure
+plot(x,y)
+%% Grid
+grid on
+%% Title
+title('Sinus')
+%% x-label
+xlabel('x')
+%% y-label
+ylabel('y')
